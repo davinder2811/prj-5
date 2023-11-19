4927148ff05021013ae29b0b9bc9f495056c19dd
davinder2811 <147605912+davinder2811@users.noreply.github.com>
2023-11-19 03:10:52 -0800

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,36 @@
+package day1_basic;
+//wap to sum two number
+/*
+ * primitive datatype (8)
+ *  -int ,char ,float ,double,boolean,byte ,long ,short  
+ * non primitive datatype 
+ * 
+ */
+public class Calculation {
+
+
+	public static void main(String[] args) {
+        
+		short a=1000; //( -32768 to 32767)
+		int x=1000;
+		char e= 'a';
+		float f= 10.4f;
+		double d= 22.33;
+		long l=223l;
+		boolean bool= true;
+		byte y=22;
+		
+		System.out.println(10+20);
+		//System = predefined class
+		//out = stream 
+		//println= method
+		//3 types of stream - in /out/err
+		
+		
+		int m; //local variable 
+		//System.out.println(m);//must be assigned first before use 
+		
+		
+	}
+
+}
